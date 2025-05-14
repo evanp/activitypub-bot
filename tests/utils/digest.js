1e5befe521ae8353e9cf99033684a55d8997654a
Evan Prodromou <evan@prodromou.name>
2025-05-13 23:06:19 -0400

    fix: better Digest and Date validation, better errors

--- tests/utils/digest.js
+++ tests/utils/digest.js
@@ -3,5 +3,5 @@ import crypto from 'node:crypto'
 export function makeDigest (body) {
   const digest = crypto.createHash('sha256')
   digest.update(body)
-  return `SHA-256=${digest.digest('base64')}`
+  return `sha-256=${digest.digest('base64')}`
 }
