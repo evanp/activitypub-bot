42eec6f0998b8a1541dd21e73d084d14a4db70cb
Evan Prodromou <evan@prodromou.name>
2025-06-03 16:35:49 -0400

    fix: httpsignature when there are parameters

--- tests/httpsignature.test.js
+++ tests/httpsignature.test.js
@@ -8,14 +8,15 @@ import Logger from 'pino'
 import { Digester } from '../lib/digester.js'
 
 describe('HTTPSignature', async () => {
-  const origin = 'https://activitypubbot.example'
+  const domain = 'activitypubbot.example'
+  const origin = `https://${domain}`
   let connection = null
   let httpSignature = null
   let logger = null
   let digester = null
   before(async () => {
     logger = Logger({
-      level: 'silent'
+      level: 'debug'
     })
     connection = new Sequelize('sqlite::memory:', { logging: false })
     await connection.authenticate()
@@ -51,6 +52,27 @@ describe('HTTPSignature', async () => {
     const publicKeyPem = await getPublicKey(username)
     const method = 'GET'
     const path = '/user/ok/outbox'
+    const result = await httpSignature.validate(publicKeyPem, signature, method, path, headers)
+    assert.ok(result)
+  })
+
+  it('can validate a signature on an URL with parameters', async () => {
+    const username = 'test'
+    const lname = 'ok'
+    const date = new Date().toUTCString()
+    const signature = await nockSignature({
+      url: `${origin}/.well-known/webfinger?resource=acct:${lname}@${domain}`,
+      date,
+      username
+    })
+    const headers = {
+      date,
+      signature,
+      host: URL.parse(origin).host
+    }
+    const publicKeyPem = await getPublicKey(username)
+    const method = 'GET'
+    const path = `/.well-known/webfinger?resource=acct:${lname}@${domain}`
     const result = await httpSignature.validate(publicKeyPem, signature, method, path, headers)
     assert.ok(result)
   })
