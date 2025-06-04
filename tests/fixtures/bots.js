61eedbf103b2f2d206ce888c344109f7517c54d9
Evan Prodromou <evan@prodromou.name>
2025-06-03 21:50:46 -0400

    test: add a lot more test bots

--- tests/fixtures/bots.js
+++ tests/fixtures/bots.js
@@ -3,5 +3,13 @@ import OKBot from '../../lib/bots/ok.js'
 
 export default {
   ok: new OKBot('ok'),
-  null: new DoNothingBot('null')
+  null: new DoNothingBot('null'),
+  test0: new DoNothingBot('test0'),
+  test1: new DoNothingBot('test1'),
+  test2: new DoNothingBot('test2'),
+  test3: new DoNothingBot('test3'),
+  test4: new DoNothingBot('test4'),
+  test5: new DoNothingBot('test5'),
+  test6: new DoNothingBot('test6'),
+  test7: new DoNothingBot('test7')
 }
