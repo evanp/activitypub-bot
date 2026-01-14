af75e724c7269bd38d0dcff8dda5cc8312335ea0
Evan Prodromou <evan@prodromou.name>
2026-01-14 15:45:34 -0500

    feat: delivery to followers with shared inbox

--- tests/routes.sharedinbox.test.js
+++ tests/routes.sharedinbox.test.js
@@ -5,7 +5,7 @@ import request from 'supertest'
 
 import { makeApp } from '../lib/app.js'
 
-import { nockSetup, nockSignature, nockFormat } from './utils/nock.js'
+import { nockSetup, nockSignature, nockFormat, makeActor } from './utils/nock.js'
 import { makeDigest } from './utils/digest.js'
 import bots from './fixtures/bots.js'
 
@@ -16,11 +16,13 @@ describe('routes.sharedinbox', async () => {
   const databaseUrl = 'sqlite::memory:'
   let app = null
   let formatter = null
+  let actorStorage = null
 
   before(async () => {
     nockSetup(remoteHost)
     app = await makeApp(databaseUrl, origin, bots, 'silent')
     formatter = app.locals.formatter
+    actorStorage = app.locals.actorStorage
   })
 
   describe('can handle an directly addressed incoming activity', async () => {
@@ -67,7 +69,6 @@ describe('routes.sharedinbox', async () => {
       assert.strictEqual(response.status, 200)
     })
     it('should appear in the inbox', async () => {
-      const { actorStorage } = app.locals
       assert.strictEqual(
         true,
         await actorStorage.isInCollection(
@@ -76,6 +77,68 @@ describe('routes.sharedinbox', async () => {
           activity
         )
       )
+    })
+  })
+
+  describe('can handle an incoming followers-only activity', async () => {
+    const username = 'actor2'
+    const botNames = ['test1', 'test2']
+    const path = '/shared/inbox'
+    const url = `${origin}${path}`
+    const date = new Date().toUTCString()
+    let response = null
+    let signature = null
+    let body = null
+    let digest = null
+    let activity = null
+    let actor = null
+    before(async () => {
+      actor = await makeActor(username, remoteHost)
+      for (const botName of botNames) {
+        await actorStorage.addToCollection(botName, 'following', actor)
+      }
+      activity = await as2.import({
+        type: 'Activity',
+        actor: actor.id,
+        id: nockFormat({ username, type: 'activity', num: 1 }),
+        to: nockFormat({ username, collection: 'followers' })
+      })
+      body = await activity.write()
+      digest = makeDigest(body)
+      signature = await nockSignature({
+        method: 'POST',
+        username,
+        url,
+        digest,
+        date
+      })
+    })
+    it('should work without an error', async () => {
+      response = await request(app)
+        .post(path)
+        .send(body)
+        .set('Signature', signature)
+        .set('Date', date)
+        .set('Host', host)
+        .set('Digest', digest)
+        .set('Content-Type', 'application/activity+json')
+      assert.ok(response)
+      await app.onIdle()
+    })
+    it('should return a 200 status', async () => {
+      assert.strictEqual(response.status, 200)
+    })
+    it('should appear in all inboxes', async () => {
+      for (const botName of botNames) {
+        assert.strictEqual(
+          true,
+          await actorStorage.isInCollection(
+            botName,
+            'inbox',
+            activity
+          )
+        )
+      }
     })
   })
 })
