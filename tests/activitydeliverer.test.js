c105ecef1c99393141468289a43014d6618e99c9
Evan Prodromou <evan@prodromou.name>
2026-02-27 20:29:39 -0500

    feat: run delivery workers in app

--- tests/activitydeliverer.test.js
+++ tests/activitydeliverer.test.js
@@ -4,17 +4,13 @@ import { createMigratedTestConnection, cleanupTestData } from './utils/db.js'
 
 import Logger from 'pino'
 
-import { ActivityHandler } from '../lib/activityhandler.js'
-import { ObjectStorage } from '../lib/objectstorage.js'
 import { KeyStorage } from '../lib/keystorage.js'
 import { UrlFormatter } from '../lib/urlformatter.js'
 import { ActivityPubClient } from '../lib/activitypubclient.js'
 import { ActorStorage } from '../lib/actorstorage.js'
 import { HTTPSignature } from '../lib/httpsignature.js'
 import { Digester } from '../lib/digester.js'
-import { ActivityDistributor } from '../lib/activitydistributor.js'
-import { Authorizer } from '../lib/authorizer.js'
-import { ObjectCache } from '../lib/objectcache.js'
+import { JobQueue } from '../lib/jobqueue.js'
 
 describe('ActivityDeliverer', async () => {
   const localHost = 'local.activitydeliverer.test'
@@ -23,10 +19,10 @@ describe('ActivityDeliverer', async () => {
   const testUsernames = ['activitydeliverertest1', 'activitydeliverertest2']
   let connection
   let actorStorage
-  let handler
   let formatter
   let logger
   let client
+  let jobQueue
   let ActivityDeliverer
   let deliverer
 
@@ -40,24 +36,11 @@ describe('ActivityDeliverer', async () => {
       remoteDomains: [remoteHost]
     })
     actorStorage = new ActorStorage(connection, formatter)
-    const objectStorage = new ObjectStorage(connection)
     const keyStorage = new KeyStorage(connection, logger)
     const signer = new HTTPSignature(logger)
     const digester = new Digester(logger)
     client = new ActivityPubClient(keyStorage, formatter, signer, digester, logger)
-    const distributor = new ActivityDistributor(client, formatter, actorStorage, logger)
-    const authz = new Authorizer(actorStorage, formatter, client)
-    const cache = new ObjectCache({ longTTL: 3600 * 1000, shortTTL: 300 * 1000, maxItems: 1000 })
-    handler = new ActivityHandler(
-      actorStorage,
-      objectStorage,
-      distributor,
-      formatter,
-      cache,
-      authz,
-      logger,
-      client
-    )
+    jobQueue = new JobQueue(connection, logger)
   })
   after(async () => {
     await cleanupTestData(connection, {
@@ -68,10 +51,10 @@ describe('ActivityDeliverer', async () => {
     await connection.close()
     connection = null
     actorStorage = null
-    handler = null
     formatter = null
     logger = null
     client = null
+    jobQueue = null
     ActivityDeliverer = null
     deliverer = null
   })
@@ -83,7 +66,7 @@ describe('ActivityDeliverer', async () => {
   })
 
   it('can initialize', async () => {
-    deliverer = new ActivityDeliverer(actorStorage, handler, formatter, logger, client)
+    deliverer = new ActivityDeliverer(actorStorage, formatter, logger, client, jobQueue)
     assert.ok(deliverer)
   })
 })
