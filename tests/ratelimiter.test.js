5854efe98548b4da088e5240125172175c37dec9
Evan Prodromou <evan@prodromou.name>
2026-03-17 18:48:45 -0400

    feat: add ratelimiter class

---
+++ tests/ratelimiter.test.js
@@ -0,0 +1,75 @@
+import { describe, it, before, after } from 'node:test'
+import assert from 'node:assert'
+
+import Logger from 'pino'
+
+import { createMigratedTestConnection, cleanupTestData } from './utils/db.js'
+
+const EPSILON = 10
+
+describe('RateLimiter', async () => {
+  const LOCAL_HOST = 'local.ratelimiter.test'
+  const REMOTE_HOST = 'remote.ratelimiter.test'
+  let logger
+  let connection
+  let RateLimiter
+  let limiter
+
+  before(async () => {
+    logger = Logger({
+      level: 'silent'
+    })
+    connection = await createMigratedTestConnection()
+    await cleanupTestData(connection, {
+      localDomain: LOCAL_HOST,
+      remoteDomains: [REMOTE_HOST]
+    })
+  })
+
+  after(async () => {
+    await cleanupTestData(connection, {
+      localDomain: LOCAL_HOST,
+      remoteDomains: [REMOTE_HOST]
+    })
+    await connection.close()
+  })
+
+  it('can import correctly', async () => {
+    RateLimiter = (await import('../lib/ratelimiter.js')).RateLimiter
+    assert.ok(RateLimiter)
+    assert.strictEqual(typeof RateLimiter, 'function')
+  })
+
+  it('can be constructed', async () => {
+    assert.ok(RateLimiter)
+    limiter = new RateLimiter(connection, logger)
+    assert.ok(limiter)
+  })
+
+  it('goes quickly on first request', async () => {
+    assert.ok(limiter)
+    const startTime = new Date()
+    await limiter.limit(REMOTE_HOST)
+    const endTime = new Date()
+    assert.ok(endTime - startTime < EPSILON)
+  })
+
+  it('accepts headers for updating the limit', async () => {
+    assert.ok(limiter)
+    const headers = new Headers()
+    headers.set('x-ratelimit-limit', 1000)
+    headers.set('x-ratelimit-remaining', 10)
+    headers.set('x-ratelimit-reset', 10)
+    await limiter.update(REMOTE_HOST, headers)
+    assert.ok(true)
+  })
+
+  it('waits about a second for the next request', async () => {
+    assert.ok(limiter)
+    const startTime = new Date()
+    await limiter.limit(REMOTE_HOST)
+    const endTime = new Date()
+    assert.ok(endTime - startTime > 500)
+    assert.ok(endTime - startTime < 2000)
+  })
+})
