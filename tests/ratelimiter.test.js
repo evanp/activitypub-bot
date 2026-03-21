7812cf49bba4e05746c08216d69c9b9a9f29f5fb
Evan Prodromou <evan@prodromou.name>
2026-03-21 14:22:10 -0400

    feat: support dates or integers for ratelimit header

--- tests/ratelimiter.test.js
+++ tests/ratelimiter.test.js
@@ -60,12 +60,34 @@ describe('RateLimiter', async () => {
     const headers = new Headers()
     headers.set('x-ratelimit-limit', 1000)
     headers.set('x-ratelimit-remaining', 3)
-    headers.set('x-ratelimit-reset', 1)
+    headers.set('x-ratelimit-reset', (new Date(Date.now() + 1000)).toISOString())
     await limiter.update(REMOTE_HOST, headers)
     assert.ok(true)
   })
 
   it('spaces out the next requests', async () => {
+    assert.ok(limiter)
+    const startTime = new Date()
+    await limiter.limit(REMOTE_HOST)
+    await limiter.limit(REMOTE_HOST)
+    await limiter.limit(REMOTE_HOST)
+    await limiter.limit(REMOTE_HOST)
+    const endTime = new Date()
+    assert.ok(endTime - startTime > 500)
+    assert.ok(endTime - startTime < 2000)
+  })
+
+  it('accepts integer header for updating the limit', async () => {
+    assert.ok(limiter)
+    const headers = new Headers()
+    headers.set('x-ratelimit-limit', 1000)
+    headers.set('x-ratelimit-remaining', 3)
+    headers.set('x-ratelimit-reset', 1)
+    await limiter.update(REMOTE_HOST, headers)
+    assert.ok(true)
+  })
+
+  it('spaces out the next requests for integer', async () => {
     assert.ok(limiter)
     const startTime = new Date()
     await limiter.limit(REMOTE_HOST)
